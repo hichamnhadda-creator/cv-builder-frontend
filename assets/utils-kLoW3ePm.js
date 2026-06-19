ff5aa9db13691eafebaf1ada3f6c7001e1a2726e
hichamnhadda-creator <hichamnhadda@gmail.com>
2026-06-19 16:03:23 +0000

    Deploying to gh-pages from @ hichamnhadda-creator/cv-builder-frontend@d5684e0922c5f79155ddc70fc6e2352cd6cfb368 🚀

--- assets/utils-kLoW3ePm.js
+++
@@ -1,1 +0,0 @@
-import{r as n}from"./index-DrwxIyW-.js";const a=e=>e?typeof e=="object"?e.name||e.label||"":e:"",f=e=>e?typeof e=="object"?e.language||e.name||"":e:"",u=e=>e&&typeof e=="object"&&e.level||"",c=e=>{const t=n.find(r=>r.value===e);return t?t.label:e};export{f as a,c as b,u as c,a as g};
